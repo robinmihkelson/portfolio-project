639ea091c3d6503070c7d814cc27277b8c90180d
robinmihkelson <rmihkelson06@gmail.com>
2024-10-24 11:26:58 +0300

    commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="relative bg-center h-screen w-full" style="background-image: url(/src/assets/6adc4fa7-2400-4ec2-873b-7b3f1a73ae7d\ 1.png);">
+    <div class="flex justify-center">
+      <div class="bg-slate-800 w-[650px] h-[75px] flex justify-center gap-6 mt-4 border-2 border-[#DAC5A7]">
+        <button class="text-[#DAC5A7] text-3xl -mt-1">arik.</button>
+        <button class="text-[#DAC5A7]">SERVICES</button>
+        <button class="text-[#DAC5A7]">WORK</button>
+        <button class="text-[#DAC5A7]">ABOUT</button>
+        <button class="text-[#DAC5A7]">BLOG</button>
+        <button class="text-[#DAC5A7]">PAGES</button>
+        <button class="w-24 h-14 bg-[#DAC5A7] mt-2">LET'S TALK</button>
+      </div>
+    </div>
+
+    <div class="text-center mt-72">
+      <h1 class="text-[#DAC5A7] font-serif text-[140px]">Web Designer</h1>
+      <h1 class="text-[#DAC5A7] italic font-serif text-[140px]">& Developer</h1>
+    </div>
+
+  </section>
+
+  <div class="bg-black w-full h-64">
+    <div class="flex justify-center pt-24">
+      <img src="/src/assets/Container.png" alt="">
+    </div>
+  </div>
+</template>
+
+
---
+++ src/components/FrontPage.vue
@@ -0,0 +1,17 @@
+<script>
+    import { ref } from "vue";
+</script>
+
+<template>
+    <div class="flex justify-center">
+      <div class="bg-slate-700 w-[650px] h-[75px] flex justify-center gap-6 mt-4 border-2 border-[#DAC5A7]">
+        <button class="text-[#DAC5A7] text-2xl">arik.</button>
+        <button class="text-[#DAC5A7]">SERVICES</button>
+        <button class="text-[#DAC5A7]">WORK</button>
+        <button class="text-[#DAC5A7]">ABOUT</button>
+        <button class="text-[#DAC5A7]">BLOG</button>
+        <button class="text-[#DAC5A7]">PAGES</button>
+        <button class="w-24 h-14 bg-[#DAC5A7] mt-2">LET'S TALK</button>
+      </div>
+    </div>
+</template>
