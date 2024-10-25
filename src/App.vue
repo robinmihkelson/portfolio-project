ced69e922fd0a4f30af8d603d587c3ca8d81be11
robinmihkelson <rmihkelson06@gmail.com>
2024-10-25 11:32:36 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,59 @@
 </script>
 
 <template>
-  <section class="relative bg-center h-screen w-full" style="background-image: url(/src/assets/6adc4fa7-2400-4ec2-873b-7b3f1a73ae7d\ 1.png);">
+  <section class="relative bg-center h-screen w-full"
+    style="background-image: url(/src/assets/6adc4fa7-2400-4ec2-873b-7b3f1a73ae7d\ 1.png);">
     <div class="flex justify-center">
-      <div class="bg-slate-800 w-[650px] h-[75px] flex justify-center gap-6 mt-4 border-2 border-[#DAC5A7]">
+      <div class="backdrop-blur-md w-[650px] h-[75px] flex justify-center gap-6 mt-4 border-2 border-[#DAC5A7]">
         <button class="text-[#DAC5A7] text-3xl -mt-1">arik.</button>
         <button class="text-[#DAC5A7]">SERVICES</button>
         <button class="text-[#DAC5A7]">WORK</button>
         <button class="text-[#DAC5A7]">ABOUT</button>
         <button class="text-[#DAC5A7]">BLOG</button>
         <button class="text-[#DAC5A7]">PAGES</button>
-        <button class="w-24 h-14 bg-[#DAC5A7] mt-2">LET'S TALK</button>
+        <button class="w-24 h-14 bg-[#DAC5A7] mt-2 hover:bg-orange-300">LET'S TALK</button>
       </div>
     </div>
 
     <div class="text-center mt-72">
-      <h1 class="text-[#DAC5A7] font-serif text-[140px]">Web Designer</h1>
-      <h1 class="text-[#DAC5A7] italic font-serif text-[140px]">& Developer</h1>
+      <h1 class="text-[#DAC5A7] font-serif text-[100px]">Web Designer</h1>
+      <h1 class="text-[#DAC5A7] italic font-serif text-[100px]">& Developer</h1>
     </div>
 
   </section>
 
-  <div class="bg-black w-full h-64">
+  <div class="bg-black w-full h-[700px]">
     <div class="flex justify-center pt-24">
       <img src="/src/assets/Container.png" alt="">
     </div>
+
+    <div class="flex flex-row justify-center gap-4 mt-32">
+      <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
+        <p class="text-[#DAC5A7] mt-8 mx-8 font-serif">01</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">WEB DESIGN</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.</p>
+        <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+          <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+        </button>
+      </div>
+
+      <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
+        <p class="text-[#DAC5A7] mt-8 mx-8 font-serif">02</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">DEVELOPMENT</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">Get custom web development solutions that are tailored to your specifications, designed to deliver a flawless user experience.</p>
+        <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+          <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+        </button>
+      </div>
+
+      <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
+        <p class="text-[#DAC5A7] mt-8 mx-8 font-serif">03</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">CONTENT & SEO</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">Proven SEO strategies that enhance your online performance, bringing you to the forefront of organic search results.</p>
+        <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+          <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+        </button>
+      </div>
+    </div>
   </div>
 </template>
-
-
