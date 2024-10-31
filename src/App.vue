32e479d3185f353d2fafdc5a6ceb023b8d0ae4f9
robinmihkelson <rmihkelson06@gmail.com>
2024-10-31 10:59:51 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -24,38 +24,75 @@
 
   </section>
 
-  <div class="bg-black w-full h-[700px]">
+  <div class="bg-black w-full h-[2000px]">
     <div class="flex justify-center pt-24">
       <img src="/src/assets/Container.png" alt="">
     </div>
 
     <div class="flex flex-row justify-center gap-4 mt-32">
       <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
-        <p class="text-[#DAC5A7] mt-8 mx-8 font-serif">01</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">WEB DESIGN</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.</p>
-        <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
-          <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
-        </button>
+        <p class="text-[#DAC5A7] mt-8 mx-8 text-opacity-30">01</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif text-opacity-65 text-2xl">WEB DESIGN</p>
+        <p class="text-[#DAC5A7] mt-1 mx-8 font-serif text-opacity-30">Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.</p>
+        <div class="flex">
+          <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+            <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+          </button>
+          <p class="text-[#DAC5A7] mt-4 -mx-5 font-serif text-opacity-65">ABOUT WEBDESIGN</p>
+        </div>
       </div>
 
       <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
-        <p class="text-[#DAC5A7] mt-8 mx-8 font-serif">02</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">DEVELOPMENT</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">Get custom web development solutions that are tailored to your specifications, designed to deliver a flawless user experience.</p>
-        <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
-          <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
-        </button>
+        <p class="text-[#DAC5A7] mt-8 mx-8 text-opacity-30">02</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif text-opacity-65 text-2xl">DEVELOPMENT</p>
+        <p class="text-[#DAC5A7] mt-1 mx-8 font-serif text-opacity-30">Get custom web development solutions that are tailored to your specifications, designed to deliver a flawless user experience.</p>
+        <div class="flex">
+          <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+            <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+          </button>
+          <p class="text-[#DAC5A7] mt-4 -mx-5 font-serif text-opacity-65">ABOUT WEBFLOW</p>
+        </div>
       </div>
 
       <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
-        <p class="text-[#DAC5A7] mt-8 mx-8 font-serif">03</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">CONTENT & SEO</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif">Proven SEO strategies that enhance your online performance, bringing you to the forefront of organic search results.</p>
-        <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
-          <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
-        </button>
+        <p class="text-[#DAC5A7] mt-8 mx-8 text-opacity-30">03</p>
+        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif text-opacity-65 text-2xl">CONTENT & SEO</p>
+        <p class="text-[#DAC5A7] mt-1 mx-8 font-serif text-opacity-30">Proven SEO strategies that enhance your online performance, bringing you to the forefront of organic search results.</p>
+        <div class="flex">
+          <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+            <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+          </button>
+          <p class="text-[#DAC5A7] mt-4 -mx-5 font-serif text-opacity-65">ABOUT SEO</p>
+        </div>
       </div>
+  </div>
+  <div class="flex flex-row justify-between mt-20 mx-36">
+    <p class="text-[#DAC5A7] text-4xl font-serif">Selected <span class="italic">Work</span></p>
+    <div class="flex items-center gap-2">
+    <button class="h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-35 border-[1px] ml-4">
+      <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+    </button>
+    <p class="text-[#DAC5A7] font-serif text-opacity-65 ml-2">SEE ALL</p>
     </div>
   </div>
+  <div class="flex flex-row justify-center gap-4 mt-4">
+    <div>
+      <img class="h-[430px] w-[610px] border-2" src="/src/assets/IMG1.png" alt="">
+    </div>
+    <div>
+      <img class="h-[430px] w-[610px] border-2" src="/src/assets/IMG2.png" alt="">
+    </div>
+  </div>
+    <div class="flex flex-row justify-center gap-4 mt-4">
+      <div>
+        <img class="h-[430px] w-[610px] border-2" src="/src/assets/IMG3.png" alt="">
+      </div>
+    <div>
+        <img class="h-[430px] w-[610px] border-2" src="/src/assets/IMG4.png" alt="">
+      </div>
+    </div>
+
+  </div>
+
+
 </template>
