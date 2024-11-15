ce84062932da992be2a8524ec15c08677606e844
robinmihkelson <rmihkelson06@gmail.com>
2024-11-15 11:30:45 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -3,103 +3,153 @@
 </script>
 
 <template>
-  <section class="relative bg-center h-screen w-full"
-    style="background-image: url(/src/assets/6adc4fa7-2400-4ec2-873b-7b3f1a73ae7d\ 1.png);">
+  <section class="relative bg-center h-screen w-full" style="background-image: url(/src/assets/smokey.png);">
     <div class="flex justify-center">
-      <div class="backdrop-blur-md w-[650px] h-[75px] flex justify-center gap-6 mt-4 border-2 border-[#DAC5A7]">
-        <button class="text-[#DAC5A7] text-3xl -mt-1">arik.</button>
-        <button class="text-[#DAC5A7]">SERVICES</button>
-        <button class="text-[#DAC5A7]">WORK</button>
-        <button class="text-[#DAC5A7]">ABOUT</button>
-        <button class="text-[#DAC5A7]">BLOG</button>
-        <button class="text-[#DAC5A7]">PAGES</button>
-        <button class="w-24 h-14 bg-[#DAC5A7] mt-2 hover:bg-orange-300">LET'S TALK</button>
+      <div class="backdrop-blur-md w-[650px] h-[75px] flex justify-center gap-6 mt-4 border-2 border-[#66FCF1]">
+        <button class="text-[#C5C6C7] text-3xl -mt-1">robin.</button>
+        <button class="text-[#C5C6C7]">SERVICES</button>
+        <button class="text-[#C5C6C7]">WORK</button>
+        <button class="text-[#C5C6C7]">ABOUT</button>
+        <button class="text-[#C5C6C7]">BLOG</button>
+        <button class="text-[#C5C6C7]">PAGES</button>
+        <button class="w-24 h-14 bg-[#66FCF1] mt-2 hover:bg-orange-300">LET'S TALK</button>
       </div>
     </div>
 
     <div class="text-center mt-72">
-      <h1 class="text-[#DAC5A7] font-serif text-[100px]">Web Designer</h1>
-      <h1 class="text-[#DAC5A7] italic font-serif text-[100px]">& Developer</h1>
+      <h1 class="text-[#66FCF1] font-serif text-[100px]">Web Designer</h1>
+      <h1 class="text-[#66FCF1] italic font-serif text-[100px]">& Developer</h1>
     </div>
 
   </section>
 
-  <div class="bg-black w-full h-[2000px]">
+  <div class="bg-[#0B0C10] w-full h-[5000px]">
     <div class="flex justify-center pt-24">
       <img src="/src/assets/Container.png" alt="">
     </div>
 
     <div class="flex flex-row justify-center gap-4 mt-32">
-      <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
-        <p class="text-[#DAC5A7] mt-8 mx-8 text-opacity-30">01</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif text-opacity-65 text-2xl">WEB DESIGN</p>
-        <p class="text-[#DAC5A7] mt-1 mx-8 font-serif text-opacity-30">Visually stunning web designs that captivate your audience by blending your brand voice and customer needs.</p>
+      <div class="bg-[#1F2833] w-[400px] h-64 bg-opacity-45">
+        <p class="text-[#66FCF1] mt-8 mx-8 text-opacity-65">01</p>
+        <p class="text-[#66FCF1] mt-4 mx-8 font-serif text-opacity-65 text-2xl">WEB DESIGN</p>
+        <p class="text-[#C5C6C7] mt-1 mx-8 font-serif text-opacity-65">Visually stunning web designs that captivate your
+          audience by blending your brand voice and customer needs.</p>
         <div class="flex">
-          <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+          <button class=" h-[30px] w-[30px] rounded-2xl border-[#C5C6C7] border-opacity-15 border-[1px] mt-3 mx-8">
             <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
           </button>
-          <p class="text-[#DAC5A7] mt-4 -mx-5 font-serif text-opacity-65">ABOUT WEBDESIGN</p>
+          <p class="text-[#66FCF1] mt-4 -mx-5 font-serif text-opacity-65">ABOUT WEBDESIGN</p>
         </div>
       </div>
 
-      <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
-        <p class="text-[#DAC5A7] mt-8 mx-8 text-opacity-30">02</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif text-opacity-65 text-2xl">DEVELOPMENT</p>
-        <p class="text-[#DAC5A7] mt-1 mx-8 font-serif text-opacity-30">Get custom web development solutions that are tailored to your specifications, designed to deliver a flawless user experience.</p>
+      <div class="bg-[#1F2833] w-[400px] h-64 bg-opacity-45">
+        <p class="text-[#66FCF1] mt-8 mx-8 text-opacity-65">02</p>
+        <p class="text-[#66FCF1] mt-4 mx-8 font-serif text-opacity-65 text-2xl">DEVELOPMENT</p>
+        <p class="text-[#C5C6C7] mt-1 mx-8 font-serif text-opacity-65">Get custom web development solutions that are
+          tailored to your specifications, designed to deliver a flawless user experience.</p>
         <div class="flex">
-          <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+          <button class=" h-[30px] w-[30px] rounded-2xl border-[#C5C6C7] border-opacity-15 border-[1px] mt-3 mx-8">
             <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
           </button>
-          <p class="text-[#DAC5A7] mt-4 -mx-5 font-serif text-opacity-65">ABOUT WEBFLOW</p>
+          <p class="text-[#66FCF1] mt-4 -mx-5 font-serif text-opacity-65">ABOUT WEBFLOW</p>
         </div>
       </div>
 
-      <div class="bg-[#DAC5A7] w-[400px] h-64 bg-opacity-15">
-        <p class="text-[#DAC5A7] mt-8 mx-8 text-opacity-30">03</p>
-        <p class="text-[#DAC5A7] mt-4 mx-8 font-serif text-opacity-65 text-2xl">CONTENT & SEO</p>
-        <p class="text-[#DAC5A7] mt-1 mx-8 font-serif text-opacity-30">Proven SEO strategies that enhance your online performance, bringing you to the forefront of organic search results.</p>
+      <div class="bg-[#1F2833] w-[400px] h-64 bg-opacity-45">
+        <p class="text-[#66FCF1] mt-8 mx-8 text-opacity-65">03</p>
+        <p class="text-[#66FCF1] mt-4 mx-8 font-serif text-opacity-65 text-2xl">CONTENT & SEO</p>
+        <p class="text-[#C5C6C7] mt-1 mx-8 font-serif text-opacity-65">Proven SEO strategies that enhance your online
+          performance, bringing you to the forefront of organic search results.</p>
         <div class="flex">
-          <button class=" h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-15 border-[1px] mt-3 mx-8">
+          <button class=" h-[30px] w-[30px] rounded-2xl border-[#C5C6C7] border-opacity-15 border-[1px] mt-3 mx-8">
             <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
           </button>
-          <p class="text-[#DAC5A7] mt-4 -mx-5 font-serif text-opacity-65">ABOUT SEO</p>
+          <p class="text-[#66FCF1] mt-4 -mx-5 font-serif text-opacity-65">ABOUT SEO</p>
         </div>
       </div>
-  </div>
-  <div class="flex flex-row justify-between mt-20 mx-36">
-    <p class="text-[#DAC5A7] text-4xl font-serif">Selected <span class="italic">Work</span></p>
-    <div class="flex items-center gap-2">
-    <button class="h-[30px] w-[30px] rounded-2xl border-[#DAC5A7] border-opacity-35 border-[1px] ml-4">
-      <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
-    </button>
-    <p class="text-[#DAC5A7] font-serif text-opacity-65 ml-2 mt-1">SEE ALL</p>
     </div>
-  </div>
-  <div class="flex flex-row justify-center gap-6 mt-10">
-    <div>
-      <section class="h-[430px] w-[610px] border-2 border-gray-800"
-    style="background-image: url(/src/assets/IMG1.png);">
-      </section>
+    <div class="flex flex-row justify-between mt-20 mx-36">
+      <p class="text-[#66FCF1] text-4xl font-serif">Selected <span class="italic">Work</span></p>
+      <div class="flex items-center gap-2">
+        <button class="h-[30px] w-[30px] rounded-2xl border-[#C5C6C7] border-opacity-35 border-[1px] ml-4">
+          <img class="mx-[5px]" src="/src/assets/ph_arrow-up-right-light.png" alt="">
+        </button>
+        <p class="text-[#66FCF1] font-serif text-opacity-65 ml-2 mt-1">SEE ALL</p>
+      </div>
     </div>
-    <div>
-      <section class="h-[430px] w-[610px] border-2 border-gray-800"
-    style="background-image: url(/src/assets/IMG2.png);">
-      </section>
+    <div class="flex flex-row justify-center gap-6 mt-10">
+      <div>
+        <section class="h-[430px] w-[610px] border-2 border-gray-800"
+          style="background-image: url(/src/assets/IMG1.png);">
+        </section>
+      </div>
+      <div>
+        <section class="h-[430px] w-[610px] border-2 border-gray-800"
+          style="background-image: url(/src/assets/IMG2.png);">
+        </section>
+      </div>
     </div>
-  </div>
     <div class="flex flex-row justify-center gap-6 mt-6">
       <div>
         <section class="h-[430px] w-[610px] border-2 border-gray-800"
-    style="background-image: url(/src/assets/IMG3.png);">
+          style="background-image: url(/src/assets/IMG3.png);">
         </section>
       </div>
-    <div>
-      <section class="h-[430px] w-[610px] border-2 border-gray-800"
-    style="background-image: url(/src/assets/IMG4.png);">
+      <div>
+        <section class="h-[430px] w-[610px] border-2 border-gray-800"
+          style="background-image: url(/src/assets/IMG4.png);">
         </section>
+
+      </div>
     </div>
+    <div class="text-center">
+      <h1 class="text-[#C5C6C7] mt-24 text-[15px] text-opacity-65">THE PROCESS</h1>
+      <p class="text-[100px] text-[#66FCF1] mt-6 font-serif">Your Website</p>
+      <p class="text-[100px] text-[#66FCF1] italic font-serif -mt-6">in 5 steps</p>
+      <h1 class="text-[#C5C6C7] text-opacity-65 text-[15px] mt-6">Our process ensures that we create a website</h1>
+      <h1 class="text-[#C5C6C7] text-opacity-65 text-[15px]">tailored to your business needs.</h1>
     </div>
 
+    <div class="flex justify-center">
+      <button class=" h-[30px] w-[30px] rounded-2xl border-[#C5C6C7] border-opacity-15 border-[1px] mt-12 mx-8">
+        <img class="mx-[5px]" src="/src/assets/ph_arrow-down-light.png" alt="">
+      </button>
+    </div>
+
+    <div class="flex flex-row-reverse">
+      <div class="bg-[#1F2833] w-[550px] h-96 bg-opacity-45 mx-24 mt-24 border-2 border-[#66FCF1] border-opacity-15">
+        <p class="text-[#C5C6C7] mt-8 mx-8 text-opacity-45">DO WE MATCH?</p>
+        <p class="text-2xl text-[#66FCF1] mt-4 mx-8">DISCOVERY CALL</p>
+        <p class="text-[#C5C6C7] w-[500px] mx-8 mt-4 text-opacity-45">Before we start, we determine if and how I can help you. What are your requirements for your new website? Why do you need a new website? What goals do you have, and what problems can we solve with a new website?</p>
+        <ul class="list-disc mx-[50px] mt-4 text-[#66FCF1]">
+          <li>We get to know each other better</li>
+          <li class="mt-4">Determine how I can best assist you</li>
+          <li class="mt-4">Understand the goals you have for your website</li>
+        </ul>
+      </div>
+
+      <div class="bg-[#1F2833] w-[550px] h-96 bg-opacity-45 mx-24 mt-[400px] border-2 border-[#66FCF1] border-opacity-15">
+        <p class="text-[#C5C6C7] mt-8 mx-8 text-opacity-45">LOREM IPSUM</p>
+        <p class="text-2xl text-[#66FCF1] mt-4 mx-8">CONCEPT & STRATEGY</p>
+        <p class="text-[#C5C6C7] w-[500px] mx-8 mt-4 text-opacity-45">Together, we develop a strategy that successfully combines your goals with the needs of your target audience. Based on this concept, I create the first wireframes and an interactive prototype. This provides us with a very good impression of the website and the user interface.</p>
+        <ul class="list-disc mx-[50px] mt-4 text-[#66FCF1]">
+          <li>UX Design</li>
+          <li class="mt-4">Wireframes</li>
+          <li class="mt-4">interactive Prototype</li>
+        </ul>
+      </div>
+
+      <div class="bg-[#1F2833] w-[550px] h-96 bg-opacity-45 mx-24 mt-[400px] border-2 border-[#66FCF1] border-opacity-15">
+        <p class="text-[#C5C6C7] mt-8 mx-8 text-opacity-45">LOREM IPSUM</p>
+        <p class="text-2xl text-[#66FCF1] mt-4 mx-8">CONCEPT & STRATEGY</p>
+        <p class="text-[#C5C6C7] w-[500px] mx-8 mt-4 text-opacity-45">Together, we develop a strategy that successfully combines your goals with the needs of your target audience. Based on this concept, I create the first wireframes and an interactive prototype. This provides us with a very good impression of the website and the user interface.</p>
+        <ul class="list-disc mx-[50px] mt-4 text-[#66FCF1]">
+          <li>UX Design</li>
+          <li class="mt-4">Wireframes</li>
+          <li class="mt-4">interactive Prototype</li>
+        </ul>
+      </div>
+    </div>
   </div>
 
 
