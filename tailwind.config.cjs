e4f5d6295812f600a18f2b28ac05519c039af737
Robin Mihkelson <145542575+robinmihkelson@users.noreply.github.com>
2025-03-26 10:36:44 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,11 @@
 module.exports = {
-  purge: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-   darkMode: false, // or 'media' or 'class'
-   theme: {
-     extend: {},
-   },
-   variants: {
-     extend: {},
-   },
-   plugins: [],
- }
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], // Kasutame "content" mitte "purge"
+  darkMode: 'media', // Või 'class', sõltuvalt vajadusest
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
