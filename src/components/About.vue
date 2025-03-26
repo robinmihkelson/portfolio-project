2411abc92e11ba0deae0cabe5482cca2274c2f84
robinmihkelson <rmihkelson06@gmail.com>
2025-03-26 10:58:39 +0200

    commit

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,7 +6,7 @@
   <section class="relative bg-center h-screen w-full bg-cover" style="background-image: url('/src/assets/smokey.png');">
     <div class="flex justify-center">
       <div class="backdrop-blur-md w-full sm:w-[650px] h-[75px] flex justify-center gap-6 mt-4 border-2 border-[#66FCF1]">
-        <router-link to="/frontpage" href class="text-[#C5C6C7] text-sm sm:text-base mt-6">robin.</router-link>
+        <router-link to="/" href class="text-[#C5C6C7] text-sm sm:text-base mt-6">robin.</router-link>
         <router-link to="/services" href class="text-[#C5C6C7] text-sm sm:text-base mt-6">SERVICES</router-link>
         <router-link to="/about" href class="text-[#C5C6C7] text-sm sm:text-base mt-6 border-b-2 border-[#66FCF1]">ABOUT</router-link>
         <router-link to="/services" href class="w-24 h-14 bg-[#66FCF1] mt-2 hover:bg-[#206e69] transition text-xs sm:text-base flex justify-center items-center">LET'S TALK</router-link>
